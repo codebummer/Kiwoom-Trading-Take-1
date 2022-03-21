a022ff78a1f49641f0f59e2282f8fcfd5d924584
codebummer <holysmokes@hanmail.net>
2022-03-21 16:00:16 +0900

    Update and rename kiwoom_control.py to kiwoom_control_2_of_2.py

--- kiwoom_control_2_of_2.py
+++ kiwoom_control_2_of_2.py
@@ -1,7 +1,7 @@
-from get_kiwoom_connection import *
+from get_kiwoom_connection_1_of_2 import *
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    kiwoom = Kiwoom()
+    kiwoom = Kiwoom_Connect()
     kiwoom.comm_connect()
     app.exec_()
