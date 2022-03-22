a419a28d07a33c8e6a4fdc01b9732e30aa03ffec
codebummer <holysmokes@hanmail.net>
2022-03-23 00:48:09 +0900

    Update DART.py

--- DART.py
+++ DART.py
@@ -1,11 +1,21 @@
-import zipfile, io
-import urllib.request
+import zipfile, io, requests
+# import urllib.request
 from cert_key import *
 
-url = 'https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key='
+# url = 'https://opendart.fss.or.kr/api/corpCode.xml?crtfc_key='
+url = 'https://opendart.fss.or.kr/api/corpCode.xml'
 param = CERT_KEY
 
-with urllib.request.urlopen(url+param) as response:
-    with zipfile.ZipFile(io.BytesIO(response.read())) as zip:
-        zip.extractall('corpCode')
+# with urllib.request.urlopen(url+param) as response:
+#     with zipfile.ZipFile(io.BytesIO(response.read())) as zip:
+#         zip.extractall('corpCode')
+#         print('All Business List Successfully Received')
+
+
+with requests.get(url, params = param) as response:
+    with zipfile.ZipFile(io.BytesIO(response.content)) as zip:
+        # response.content shows it contains bytes strings. 
+        # However, it shows it is not in a zip format, but in a XML format
+        # So, make a statement to handle response as a XML format
         print('All Business List Successfully Received')
+
