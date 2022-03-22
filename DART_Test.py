71968b46d18772079a0ff18bd54acfb683c792f2
codebummer <holysmokes@hanmail.net>
2022-03-22 21:43:20 +0900

    Update DART_Test.py

--- DART_Test.py
+++ DART_Test.py
@@ -1,10 +1,11 @@
 import requests
 import sqlite3
 import pandas as pd
+from cert_key import *
 
 url = 'https://opendart.fss.or.kr/api/fnlttSinglAcnt.json'
 params = {
-    'crtfc_key' : '1a466b948211ddfe0d373e34d3f7dd39e22439de',
+    'crtfc_key' : CERT_KEY,
     'corp_code' : '00126380',
     'bsns_year' : '2018',
     'reprt_code' : '11011'
