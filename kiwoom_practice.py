e3a351e16852fdae2efb2ecd87651068b7ebfa7a
codebummer <holysmokes@hanmail.net>
2022-11-16 23:44:28 +0900

    Update kiwoom_practice.py

--- kiwoom_practice.py
+++ kiwoom_practice.py
@@ -13,8 +13,6 @@ class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__()
         
-        # self.KOSPI_list = pd.DataFrame()
-
         self.reset()
         self.OCX_available()      
         self._event_handlers()
@@ -30,7 +28,6 @@ class Kiwoom(QAxWidget):
     def reset(self):
         self.remaining_data = False
         self.ohlcva = {'Date':[], 'Open':[], 'High':[], 'Low':[], 'Close':[], 'Volume':[], 'Amount':[]}
-        # self.db_sql('KOSPI')
    
     def account_info(self):
         account_num = self.dynamicCall('GetLoginInfo(QString)', ['ACCNO'])
@@ -43,10 +40,6 @@ class Kiwoom(QAxWidget):
         for ticker in tickers:
             stock = self.dynamicCall('GetMasterCodeName(QString)', [ticker])
             stock_list[ticker] = [stock]
-
-        # self.KOSPI_list = pd.DataFrame(stock_list)
-        # print(stock_list)
-        # print(self.KOSPI_list)
         return stock_list
     
     def set_input_value(self, tr_name, tr_value):
@@ -56,11 +49,6 @@ class Kiwoom(QAxWidget):
         self.dynamicCall('CommRQData(QString, QString, int, QString)', rqname, trcode, prenext, scrno)
         self.tr_event_loop = QEventLoop()
         self.tr_event_loop.exec_()
-
-    # def db_sql(self, table):
-    #     dbfile = sqlite3.connect(r'D:\myProjects\myKiwoom\KOSPI.db')
-    #     cursor = dbfile.cursor()
-    #     self.KOSPI_list.to_sql(table, cursor, if_exists='replace')
 
     def _login(self):
         self.dynamicCall('CommConnect')
@@ -87,6 +75,10 @@ class Kiwoom(QAxWidget):
             print('scrno, rqname, trcode, recordname, prenext, unused1, unused2, unused3, unused4: \n',\
                  scrno, rqname, trcode, recordname, prenext, unused1, unused2, unused3, unused4)
             self._opt10081(rqname, trcode)
+        elif rqname == 'OPT10079':
+            print('scrno, rqname, trcode, recordname, prenext, unused1, unused2, unused3, unused4: \n',\
+                 scrno, rqname, trcode, recordname, prenext, unused1, unused2, unused3, unused4)
+            self._opt10079(rqname, trcode)
 
         try:
             self.tr_event_loop.exit()
@@ -102,43 +94,95 @@ class Kiwoom(QAxWidget):
         data_cnt = self._get_repeat_cont(trcode, '주식일봉차트')
 
         for idx in range(data_cnt):
-            self.ohlcva['Date'].append(self._get_comm_data(trcode, rqname, idx, '일자'))
-            self.ohlcva['Open'].append(int(self._get_comm_data(trcode, rqname, idx, '시가')))
-            self.ohlcva['High'].append(int(self._get_comm_data(trcode, rqname, idx, '고가')))
-            self.ohlcva['Low'].append(int(self._get_comm_data(trcode, rqname, idx, '저가')))
-            self.ohlcva['Close'].append(int(self._get_comm_data(trcode, rqname, idx, '현재가')))
-            self.ohlcva['Volume'].append(int(self._get_comm_data(trcode, rqname, idx, '거래량')))
-            self.ohlcva['Amount'].append(int(self._get_comm_data(trcode, rqname, idx, '거래대금')))   
-               
+            date = self._get_comm_data(trcode, rqname, idx, '일자')
+            open = self._get_comm_data(trcode, rqname, idx, '시가')
+            high = self._get_comm_data(trcode, rqname, idx, '고가')
+            low = self._get_comm_data(trcode, rqname, idx, '저가')
+            close = self._get_comm_data(trcode, rqname, idx, '현재가')
+            volume = self._get_comm_data(trcode, rqname, idx, '거래량')
+            amount = self._get_comm_data(trcode, rqname, idx, '거래대금')
+
+            self.ohlcva['Date'].append(date)
+            self.ohlcva['Open'].append(int(open))
+            self.ohlcva['High'].append(int(high))
+            self.ohlcva['Low'].append(int(low))
+            self.ohlcva['Close'].append(int(close))
+            self.ohlcva['Volume'].append(int(volume))
+            self.ohlcva['Amount'].append(int(amount))
+
+    def _opt10079(self, rqname, trcode):
+        data_cnt = self._get_repeat_cont(trcode, '주식틱차트')   
+
+        for idx in range(data_cnt):
+            date = self._get_comm_data(trcode, rqname, idx, '체결시간')
+            open = self._get_comm_data(trcode, rqname, idx, '시가')
+            high = self._get_comm_data(trcode, rqname, idx, '고가')
+            low = self._get_comm_data(trcode, rqname, idx, '저가')
+            close = self._get_comm_data(trcode, rqname, idx, '현재가')
+            volume = self._get_comm_data(trcode, rqname, idx, '거래량')
+
+            self.ohlcva['Date'].append(date)
+            self.ohlcva['Open'].append(int(open))
+            self.ohlcva['High'].append(int(high))
+            self.ohlcva['Low'].append(int(low))
+            self.ohlcva['Close'].append(int(close))
+            self.ohlcva['Volume'].append(int(volume))
+            self.ohlcva['Amount'].append(None)
+           
 
     def _get_comm_data(self, trcode, rqname, idx, itemname):
         return self.dynamicCall('GetCommData(QString, QString, int, QSTring)', trcode, rqname, idx, itemname).strip()
 
-    def order_daily_chart(self, code, date, recordtype=1):
-        self.set_input_value('종목코드', code)
+    def request_daily_chart(self, stockcode, date, pricetype=1):
+        self.set_input_value('종목코드', stockcode)
         self.set_input_value('기준일자', date)
-        self.set_input_value('수정주가구분', recordtype)
-        self.comm_rq_data('OPT10081', 'opt10081', 1, '0101')
+        self.set_input_value('수정주가구분', pricetype)
+        self.comm_rq_data('OPT10081', 'opt10081', 0, '0001')
 
         while self.remaining_data == True:
             time.sleep(TR_REQ_TIME_INTERVAL)
-            self.set_input_value('종목코드', code)
+            self.set_input_value('종목코드', stockcode)
             self.set_input_value('기준일자', date)
-            self.set_input_value('수정주가구분', recordtype)
-            self.comm_rq_data('OPT10081', 'opt10081', 2, '0101')
+            self.set_input_value('수정주가구분', pricetype)
+            self.comm_rq_data('OPT10081', 'opt10081', 2, '0001')
+    
+    def request_tick_chart(self, stockcode, ticktime=1, pricetype=1):
+        self.set_input_value('종목코드', stockcode)
+        self.set_input_value('틱범위', ticktime)
+        self.set_input_value('수정주가구분', pricetype)
+        self.comm_rq_data('OPT10079', 'opt10079', 0, '0001')
+
+        while self.remaining_data == True:
+            time.sleep(TR_REQ_TIME_INTERVAL)
+            self.set_input_value('종목코드', stockcode)
+            self.set_input_value('틱범위', ticktime)
+            self.set_input_value('수정주가구분', pricetype)
+            self.comm_rq_data('OPT10079', 'opt10079', 2, '0001')
 
 def save_in_sq(tablename, df):
-    path = r'D:\myProjects\myKiwoom\algotrading_data.db'
+    # ticktime = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')    
+    # path = f'D:/myProjects/myKiwoom/algotrading_data_{ticktime}.db'
+    path = r'D:\myProjects\myKiwoom\039490.db'
     with sqlite3.connect(path) as file: 
         df.to_sql(tablename, file, if_exists='replace')
  
 app = QApplication(sys.argv)
 
 kiwoom = Kiwoom()
-kiwoom.order_daily_chart('039490', '20170224')
+
+kiwoom.request_daily_chart('039490', '20170224')
 df = pd.DataFrame(kiwoom.ohlcva, index=kiwoom.ohlcva['Date'])
 print(df)
-save_in_sq('039490', df)
+save_in_sq('039490_Daily', df)
+kiwoom.reset()
+df = pd.DataFrame()
+
+kiwoom.request_tick_chart('039490')
+df = pd.DataFrame(kiwoom.ohlcva, index=kiwoom.ohlcva['Date'])
+print(df)
+save_in_sq('03940_Tick', df)
+kiwoom.reset()
+
 
 # app.exec_()
 
